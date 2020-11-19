1ec3ff910a277d1901d0a061ea63d799cf301936
Ahmed Ben Zineb <>
2020-11-19 20:16:08 +0100

    Updates

---
+++ precache-manifest.753906f8c6cbc30474aff4af498e34ee.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f6d3d48f37936dd3570292eaf13e8e",
+    "url": "/fractal-client/index.html"
+  },
+  {
+    "revision": "6edd66c5d671549e0563",
+    "url": "/fractal-client/static/css/3.b6560306.chunk.css"
+  },
+  {
+    "revision": "bc9881ea9b4e602170c7",
+    "url": "/fractal-client/static/css/6.a5c50a63.chunk.css"
+  },
+  {
+    "revision": "361cba26b714849f29b6",
+    "url": "/fractal-client/static/css/main.c337689d.chunk.css"
+  },
+  {
+    "revision": "a4927e9af1de4e110c6e",
+    "url": "/fractal-client/static/js/0.38975613.chunk.js"
+  },
+  {
+    "revision": "820447ba06cb091971a40064982db590",
+    "url": "/fractal-client/static/js/0.38975613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b587de6e2895475458",
+    "url": "/fractal-client/static/js/1.b574c21c.chunk.js"
+  },
+  {
+    "revision": "66e1386a9006ac1a991a",
+    "url": "/fractal-client/static/js/10.4da10d48.chunk.js"
+  },
+  {
+    "revision": "76b29c5db0f81e0d80a2",
+    "url": "/fractal-client/static/js/11.5d488bcf.chunk.js"
+  },
+  {
+    "revision": "722abc73aaf16c0328a5",
+    "url": "/fractal-client/static/js/12.bec2bd22.chunk.js"
+  },
+  {
+    "revision": "260cbf9025ba18b58060",
+    "url": "/fractal-client/static/js/13.2ffedc54.chunk.js"
+  },
+  {
+    "revision": "c4fa26404fcd622de21d",
+    "url": "/fractal-client/static/js/14.e2f03f23.chunk.js"
+  },
+  {
+    "revision": "b4c47e4d66909a7a1910",
+    "url": "/fractal-client/static/js/15.18f5095b.chunk.js"
+  },
+  {
+    "revision": "a78b811010401ab3cf6c",
+    "url": "/fractal-client/static/js/16.af8fd357.chunk.js"
+  },
+  {
+    "revision": "cdecbe02a4e3fb4ce6c5",
+    "url": "/fractal-client/static/js/17.148e3f6b.chunk.js"
+  },
+  {
+    "revision": "bf4a4c5a5c10da65ddaa",
+    "url": "/fractal-client/static/js/18.3e761f1f.chunk.js"
+  },
+  {
+    "revision": "1bea19138632142ff80d",
+    "url": "/fractal-client/static/js/19.97c2c7d0.chunk.js"
+  },
+  {
+    "revision": "98d63670d762b3605703",
+    "url": "/fractal-client/static/js/2.b2f786b2.chunk.js"
+  },
+  {
+    "revision": "946d24fc1cfcc58f26b9",
+    "url": "/fractal-client/static/js/20.b87d3a30.chunk.js"
+  },
+  {
+    "revision": "4e8db56ce8fd994992ac",
+    "url": "/fractal-client/static/js/21.e75b1fea.chunk.js"
+  },
+  {
+    "revision": "113b9251b743e4eacc30",
+    "url": "/fractal-client/static/js/22.e9e14afa.chunk.js"
+  },
+  {
+    "revision": "6edd66c5d671549e0563",
+    "url": "/fractal-client/static/js/3.94920380.chunk.js"
+  },
+  {
+    "revision": "bc9881ea9b4e602170c7",
+    "url": "/fractal-client/static/js/6.01ae755c.chunk.js"
+  },
+  {
+    "revision": "73bac90d1cda1baf82f46b2f9f95d07a",
+    "url": "/fractal-client/static/js/6.01ae755c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fac56abfd2365ba521",
+    "url": "/fractal-client/static/js/7.96c73796.chunk.js"
+  },
+  {
+    "revision": "06a90d95064c465a2ffc",
+    "url": "/fractal-client/static/js/8.ac0e0ef6.chunk.js"
+  },
+  {
+    "revision": "56846eab171d582bc3cb",
+    "url": "/fractal-client/static/js/9.ddf3d7e4.chunk.js"
+  },
+  {
+    "revision": "361cba26b714849f29b6",
+    "url": "/fractal-client/static/js/main.11046a3d.chunk.js"
+  },
+  {
+    "revision": "1f6b9dc006d182b7850b",
+    "url": "/fractal-client/static/js/runtime-main.28f43d88.js"
+  },
+  {
+    "revision": "329b51cd449a3783a0ddd87e32b1ddac",
+    "url": "/fractal-client/static/media/013-lifeline-1.329b51cd.png"
+  },
+  {
+    "revision": "4dbf2dcda38a1936b51b83df87ceb971",
+    "url": "/fractal-client/static/media/015-lifeline-2.4dbf2dcd.png"
+  },
+  {
+    "revision": "51dacda9525c739a8d0e675b3102c55f",
+    "url": "/fractal-client/static/media/044-heart-6.51dacda9.png"
+  },
+  {
+    "revision": "07868a0df7feab38c150b4e2a0fe7f34",
+    "url": "/fractal-client/static/media/052-doctor-1.07868a0d.png"
+  },
+  {
+    "revision": "7bc7a90af12b7d1ae0a1a3704af5d386",
+    "url": "/fractal-client/static/media/064-medical-2.7bc7a90a.png"
+  },
+  {
+    "revision": "fddc9ae5dfce7ac5557eef79cca47a86",
+    "url": "/fractal-client/static/media/073-patient.fddc9ae5.png"
+  },
+  {
+    "revision": "1c91307e571dc9682c1d955ebc5d5222",
+    "url": "/fractal-client/static/media/075-records.1c91307e.png"
+  },
+  {
+    "revision": "f321826cafb165a4addffd3c84076bfe",
+    "url": "/fractal-client/static/media/089-tablets-1.f321826c.png"
+  },
+  {
+    "revision": "9dbcc95df5c86335f0e083c353c51239",
+    "url": "/fractal-client/static/media/NoirPro-Bold.9dbcc95d.woff"
+  },
+  {
+    "revision": "bb9d0cc624e14a907449e5ea1f602186",
+    "url": "/fractal-client/static/media/NoirPro-Bold.bb9d0cc6.woff2"
+  },
+  {
+    "revision": "d956581657d758fdda26a51e591a8a85",
+    "url": "/fractal-client/static/media/NoirPro-Bold.d9565816.eot"
+  },
+  {
+    "revision": "7194b3b5891959a3e98285e89bdc01c6",
+    "url": "/fractal-client/static/media/NoirPro-Heavy.7194b3b5.eot"
+  },
+  {
+    "revision": "873c8fa8f69d6a2118f1119e1e80eb74",
+    "url": "/fractal-client/static/media/NoirPro-Heavy.873c8fa8.woff2"
+  },
+  {
+    "revision": "a302e546bfe8cef2a36504d844542fa4",
+    "url": "/fractal-client/static/media/NoirPro-Heavy.a302e546.woff"
+  },
+  {
+    "revision": "251883125165605d8c409595ff236734",
+    "url": "/fractal-client/static/media/NoirPro-Light.25188312.woff2"
+  },
+  {
+    "revision": "bbd56c69aef4c37772e2305a2c2cd422",
+    "url": "/fractal-client/static/media/NoirPro-Light.bbd56c69.woff"
+  },
+  {
+    "revision": "defbb5912484b376c75f8235e1830b4c",
+    "url": "/fractal-client/static/media/NoirPro-Light.defbb591.eot"
+  },
+  {
+    "revision": "04b5d842dfad0fbb345cda0473c72d0e",
+    "url": "/fractal-client/static/media/NoirPro-Medium.04b5d842.woff"
+  },
+  {
+    "revision": "3d82e791b707535b54dd0d68ba38cdbc",
+    "url": "/fractal-client/static/media/NoirPro-Medium.3d82e791.woff2"
+  },
+  {
+    "revision": "6d74487df6dc4e67a2f4c3fd8650362c",
+    "url": "/fractal-client/static/media/NoirPro-Medium.6d74487d.eot"
+  },
+  {
+    "revision": "752c13e1b881564f5f54afa1a4859492",
+    "url": "/fractal-client/static/media/NoirPro-Regular.752c13e1.eot"
+  },
+  {
+    "revision": "83b986718958eae204ae595909a4d66e",
+    "url": "/fractal-client/static/media/NoirPro-Regular.83b98671.woff"
+  },
+  {
+    "revision": "fe70663122656a5c59de60232d8be61f",
+    "url": "/fractal-client/static/media/NoirPro-Regular.fe706631.woff2"
+  },
+  {
+    "revision": "1bc66a4b5cd48c729a854842fd066a1b",
+    "url": "/fractal-client/static/media/NoirPro-SemiBold.1bc66a4b.woff"
+  },
+  {
+    "revision": "ac97525f7e84a8b62907058d6aa4e93c",
+    "url": "/fractal-client/static/media/NoirPro-SemiBold.ac97525f.eot"
+  },
+  {
+    "revision": "e928d2a22faf75b6884fd3b4763bdcee",
+    "url": "/fractal-client/static/media/NoirPro-SemiBold.e928d2a2.woff2"
+  },
+  {
+    "revision": "52bf5ce3076f1e0627e148322a2048eb",
+    "url": "/fractal-client/static/media/a10.52bf5ce3.png"
+  },
+  {
+    "revision": "3ae38faf46698e975075e84bbcf16381",
+    "url": "/fractal-client/static/media/a5.3ae38faf.png"
+  },
+  {
+    "revision": "58d1c1cf4e83763b65415a10b12f4579",
+    "url": "/fractal-client/static/media/a6.58d1c1cf.png"
+  },
+  {
+    "revision": "74a075d7394c76400c3c0a8b3f78056e",
+    "url": "/fractal-client/static/media/a7.74a075d7.png"
+  },
+  {
+    "revision": "6c65f1674f58c7477ea9de40698c7250",
+    "url": "/fractal-client/static/media/domnic-brown.6c65f167.png"
+  },
+  {
+    "revision": "5360c96235b6b595dd9d34493d3b421f",
+    "url": "/fractal-client/static/media/e-health-logo-white-h.5360c962.png"
+  },
+  {
+    "revision": "00fcca8e4896ea70aeaf79cc01482b54",
+    "url": "/fractal-client/static/media/gaxon.00fcca8e.ttf"
+  },
+  {
+    "revision": "32cebc42775933015139f56793bcdca8",
+    "url": "/fractal-client/static/media/gaxon.32cebc42.eot"
+  },
+  {
+    "revision": "36251c7bf6634a7cd87cf02935463f61",
+    "url": "/fractal-client/static/media/gaxon.36251c7b.woff"
+  },
+  {
+    "revision": "b1a63de8de332478102c86d8481bafc2",
+    "url": "/fractal-client/static/media/gaxon.b1a63de8.svg"
+  },
+  {
+    "revision": "274096efb385a775d67eb1cdbcc15134",
+    "url": "/fractal-client/static/media/john-smith.274096ef.png"
+  },
+  {
+    "revision": "58a1d28fbfca3a2da75a2836835e4289",
+    "url": "/fractal-client/static/media/loader.58a1d28f.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/fractal-client/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/fractal-client/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/fractal-client/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/fractal-client/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/fractal-client/static/media/sprite-flags-24x24.7ed12ca4.png"
+  }
+]);
